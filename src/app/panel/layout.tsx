326ea4cec27b40a1c0c170394ec82c037d17279e
Nicolás Hernán Silva <koppeks.contactos@gmail.com>
2026-01-06 20:49:02 -0300

    feat: rework user menu with dropdown and avatar components; remove ButtonLogout (#18)

--- src/app/panel/layout.tsx
+++ src/app/panel/layout.tsx
@@ -1,17 +1,28 @@
 'use client';
 
-import { ButtonLogout } from '@/components/custom/ButtonLogout';
 import {
   NavigationMenu,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
 } from '@radix-ui/react-navigation-menu';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 import { useSession } from '../../providers/RouteFetchProvider';
 import '../globals.css';
 import { navigationMenuTriggerStyle } from '@/components/ui/navigation-menu';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
+import { useRouter } from 'next/navigation';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
+import API_BASE from '@/lib/endpoint';
 
 export default function PanelLayout({
   children,
@@ -19,16 +30,41 @@ export default function PanelLayout({
   children: React.ReactNode;
 }>) {
   const { user } = useSession();
+  const router = useRouter();
 
   return (
     <div>
-      <header className="w-full py-4! flex items-center justify-around">
+      <header className="w-[70vw] m-auto py-4! flex items-center justify-between">
         <div>
           <h1 className="text-primary">Sistema Contable</h1>
         </div>
         <div className="flex items-center gap-4">
-          <p>{user?.email}</p>
-          <ButtonLogout />
+          <DropdownMenu modal={false}>
+            <DropdownMenuTrigger asChild>
+              <Avatar className='cursor-pointer'>
+                <AvatarFallback className='bg-secondary text-primary font-bold'>{user?.email.slice(0, 2)}</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-40" align="end">
+              <DropdownMenuLabel>Mi cuenta</DropdownMenuLabel>
+              <DropdownMenuGroup>
+                <DropdownMenuItem className='cursor-pointer'>
+                  Perfil
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem className='cursor-pointer' onClick={async () => {
+                  const res = await fetch(`${API_BASE}/users/logout`, {
+                    method: 'POST',
+                    credentials: 'include',
+                  });
+                  console.log('Sesión cerrada:', res);
+                  if(res.ok) router.refresh();
+              }}>
+                Cerrar sesión
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </header>
       {user?.role == 'ADMIN' ? (
