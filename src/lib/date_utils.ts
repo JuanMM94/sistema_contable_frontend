2ec0a8ee869414135472d44c4ce39a4e54c4ad8d
Nicolás Hernán Silva <koppeks.contactos@gmail.com>
2026-01-07 00:04:47 -0300

    feat: update input placeholders and improve date formatting functions (#19)
    * feat: update input placeholders and improve date formatting functions
    
    * Update FormNewMovement.tsx

--- src/lib/date_utils.ts
+++ src/lib/date_utils.ts
@@ -10,10 +10,17 @@ export function formatShortDate(date: Date | undefined) {
     return '';
   }
 
-  const day = String(date!.getDate()).padStart(2, '0');
-  const month = String(date!.getMonth() + 1).padStart(2, '0');
-  const year = String(date!.getFullYear()).slice(-2);
+  const day = String(date!.getUTCDate()).padStart(2, '0');
+  const month = String(date!.getUTCMonth() + 1).padStart(2, '0');
+  const year = String(date!.getUTCFullYear()).slice(-2);
 
+  return `${day}/${month}/${year}`;
+}
+
+export function formatToLocaleDate(date: Date) {
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = String(date.getFullYear()).padStart(4, '0');
   return `${day}/${month}/${year}`;
 }
 
@@ -21,7 +28,6 @@ export function formatISODate(date: Date) {
   const day = String(date.getDate()).padStart(2, '0');
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const year = String(date.getFullYear()).padStart(4, '0');
-
   return `${year}-${month}-${day}`;
 }
 
