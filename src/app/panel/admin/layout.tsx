76c31c9398b9521ae3438b8c411a4d3defc12059
Nicolas Hernan Silva <koppeks.contactos@gmail.com>
2026-01-08 17:04:13 -0300

    fix: remove unnecessary margin from admin pages and add it to layout

--- src/app/panel/admin/layout.tsx
+++ src/app/panel/admin/layout.tsx
@@ -7,7 +7,7 @@ export default function AdminLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
+    <div className='py-6'>
       <AdminFetchProvider>{children}</AdminFetchProvider>
     </div>
   );
