557527913ccc90efef46dd30e674d7a7aa619529
Koppeks <koppeks.contactos@gmail.com>
2026-02-02 17:13:36 -0300

    fix: standardize styles across components and remove unused CSS files

--- src/app/panel/ultimos-movimientos/page.tsx
+++ src/app/panel/ultimos-movimientos/page.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import styles from '../../page.module.css';
-import { Splitter } from '@/components/custom/Splitter';
 import { ListMovementsUser } from '@/components/custom/ListMovements';
 import { useSession } from '@/providers/RouteFetchProvider';
 import { MoveLeft } from 'lucide-react';
@@ -9,6 +7,7 @@ import { useSearchParams } from 'next/navigation';
 import { ButtonGroup } from '@/components/ui/button-group';
 import { Button } from '@/components/ui/button';
 import { useState } from 'react';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
 export default function Page() {
   const { user, loading } = useSession();
@@ -20,42 +19,54 @@ export default function Page() {
     : user?.movements;
 
   return (
-    <div className={styles.dashboard}>
-      <div className="w-[70vw] p-4 flex flex-col gap-8">
-        <div
-          className="flex flex-row items-center gap-2 mb-4 cursor-pointer"
-          onClick={() => history.back()}
-        >
-          <MoveLeft size={24} />
-          <h5>Volver</h5>
-        </div>
-        <div className={styles.information_container}>
-          <section className={styles.card_section}>
-            {loading ? (
-              <></>
-            ) : (
-              <section className={styles.table_section}>
-                <h4>Últimos movimientos</h4>
-                <div className="flex justify-center">
-                  <ButtonGroup>
-                    <Button onClick={() => setFilterBy(null)} variant="outline" size="sm">
-                      Todos
-                    </Button>
-                    <Button onClick={() => setFilterBy('ARS')} variant="outline" size="sm">
-                      ARS
-                    </Button>
-                    <Button onClick={() => setFilterBy('USD')} variant="outline" size="sm">
-                      USD
-                    </Button>
-                  </ButtonGroup>
-                </div>
-                <ListMovementsUser initialMovements={filteredMovements ?? []} />
-              </section>
-            )}
-          </section>
-        </div>
+    <div className="lg:w-[70vw] w-[90vw] p-4 flex flex-col gap-8 m-auto">
+      <div
+        className="flex flex-row items-center gap-2 mb-4 cursor-pointer"
+        onClick={() => history.back()}
+      >
+        <MoveLeft size={24} />
+        <h5>Volver</h5>
       </div>
-      <Splitter />
+      {loading ? (
+        <></>
+      ) : (
+        <Card>
+          <CardHeader>
+            <CardTitle>Últimos movimientos</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="flex justify-center">
+              <ButtonGroup>
+                <Button
+                  onClick={() => setFilterBy(null)}
+                  variant="outline"
+                  size="sm"
+                  className={filterBy === null ? 'text-primary' : 'opacity-70'}
+                >
+                  Todos
+                </Button>
+                <Button
+                  onClick={() => setFilterBy('ARS')}
+                  variant="outline"
+                  size="sm"
+                  className={filterBy === 'ARS' ? 'text-primary' : 'opacity-70'}
+                >
+                  ARS
+                </Button>
+                <Button
+                  onClick={() => setFilterBy('USD')}
+                  variant="outline"
+                  size="sm"
+                  className={filterBy === 'USD' ? 'text-primary' : 'opacity-70'}
+                >
+                  USD
+                </Button>
+              </ButtonGroup>
+            </div>
+            <ListMovementsUser initialMovements={filteredMovements ?? []} />
+          </CardContent>
+        </Card>
+      )}
     </div>
   );
 }
