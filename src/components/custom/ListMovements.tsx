d3eeaed74c05e814199507c38eadd1b928730160
Nicolás Hernán Silva <koppeks.contactos@gmail.com>
2026-02-03 00:58:33 -0300

    Update ListMovements.tsx

--- src/components/custom/ListMovements.tsx
+++ src/components/custom/ListMovements.tsx
@@ -49,56 +49,6 @@ function ListMovementsAdmin() {
   ] as Column<Movement>[];
 
   return <MovementTable movements={!movements ? [] : movements} columns={displayColumns} />;
-
-  // return (
-  //   <Table className="w-full">
-  //     <TableHeader>
-  //       <TableRow>
-  //         <TableHead className="w-[100px]">Id de factura</TableHead>
-  //         <TableHead>Fecha</TableHead>
-  //         <TableHead>Usuario</TableHead>
-  //         <TableHead>Cliente</TableHead>
-  //         <TableHead>Estado</TableHead>
-  //         <TableHead>Método</TableHead>
-  //         <TableHead>Moneda</TableHead>
-  //         <TableHead>Tipo</TableHead>
-  //         <TableHead>Nota</TableHead>
-  //         <TableHead className="text-right">Cantidad</TableHead>
-  //         <TableHead className="text-center">Acciones</TableHead>
-  //       </TableRow>
-  //     </TableHeader>
-  //     <TableBody>
-  //       {rows.map((movement) => {
-  //         return (
-  //           <TableRow key={movement.id}>
-  //             <TableCell className="font-medium">{movement.id.slice(0, 8)}...</TableCell>
-  //             <TableCell className="font-medium">
-  //               {formatToLocaleDate(new Date(movement.date))}
-  //             </TableCell>
-  //             <TableCell className="font-medium">{movement.account.user.name}</TableCell>
-  //             <TableCell className="font-medium">{movement.payer}</TableCell>
-  //             <TableCell>{getPaymentStatusLabel(movement.status)}</TableCell>
-  //             <TableCell>{getPaymentMethodLabel(movement.method)}</TableCell>
-  //             <TableCell>{movement.currency}</TableCell>
-  //             <TableCell>{getPaymentTypeLabel(movement.type)}</TableCell>
-  //             <TableCell>{movement.concept}</TableCell>
-  //             <TableCell
-  //               className={`text-right ${movement.type === 'EGRESS' ? 'text-red-600' : 'text-green-600'}`}
-  //             >
-  //               {currencyFormatter(movement.amount, 'es-AR', movement.currency, true)}
-  //             </TableCell>
-  //             <TableCell className="text-center">
-  //               <div className="inline-flex items-center justify-center gap-3">
-  //                 <EditMovementDialog movement={movement} />
-  //
-  //               </div>
-  //             </TableCell>
-  //           </TableRow>
-  //         );
-  //       })}
-  //     </TableBody>
-  //   </Table>
-  // );
 }
 
 export { ListMovementsUser, ListMovementsAdmin };
